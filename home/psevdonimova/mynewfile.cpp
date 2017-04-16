70d33b8d608f16950f4013c30e4f51d4f738c61c
Psevdonimova <psevdonimova7@gmail.com>
2017-04-16 18:08:41 +0300

    Update mynewfile.cpp
    First (if) and third (else if) conditions were changed.

--- home/psevdonimova/mynewfile.cpp
+++ home/psevdonimova/mynewfile.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main() {
 for (int i = 0; i < 120; i++) {
-if (0 == (i%3u))
+if (0 == i%3u && 0 != i%2u)
 std::cout << i << " : Fizz";
 else if (0 == i%5u)
 std::cout << i << " : Buzz";
-else if (0 == i%2u)
+else if (0 == i%2u && 0 != i%3u)
 std::cout << i << " : Fuuu";
 else
 std::cout << i;
