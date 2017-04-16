05173c49a873c45b39b3ab8dfc35e0bdb4fc8936
Psevdonimova <psevdonimova7@gmail.com>
2017-04-16 21:05:35 +0300

    Update mynewfile.cpp
    300 numbers for more representational programm.

--- home/psevdonimova/mynewfile.cpp
+++ home/psevdonimova/mynewfile.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 using namespace std;
 int main() {
-for (int i = 0; i < 160; i++) {
+for (int i = 0; i < 300; i++) {
 if (0 == i%3u && 0 != i%2u)
 std::cout << i << " : Fizz";
 else if (0 == i%5u)
@@ -10,7 +10,7 @@ std::cout << i << " : Buzz";
 else if (0 == i%2u && 0 != i%3u)
 std::cout << i << " : Fuuu";
 else
-std::cout << i << " : Prime number";
+std::cout << i;
 std::cout <<std::endl;
 }
 }
